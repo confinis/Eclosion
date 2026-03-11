9d928755a41142c3cad227930cfdd9d810e3f252
confinis <76611195+confinis@users.noreply.github.com>
2026-03-11 07:19:15 +0000

    setup

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdbool.h>
+#include <SDL3/SDL.h>
+
+int main() 
+{
+    std::cout << "eclosion\n" << std::endl;
+    return 0;
+}
+
+bool init_subsystems(void){};
+bool runtime(void){};
